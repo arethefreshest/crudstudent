76eed13ce21494fe1861418caf8455e60dfe7864
arethefreshest <doffen.are@gmail.com>
2024-02-29 11:31:27 +0100

    Initial Commit

---
+++ firebaseConfig.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC31hb6NDFbny1rZj5TfpOvalhnGPiGnDY",
+    authDomain: "crud-student-management.firebaseapp.com",
+    projectId: "crud-student-management",
+    storageBucket: "crud-student-management.appspot.com",
+    messagingSenderId: "533723340092",
+    appId: "1:533723340092:web:de06dea5fcc9fa1d0dd239",
+    measurementId: "G-CYK9BENWYG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+// Export any objects needed elsewhere in the app
+// export { auth } - example
